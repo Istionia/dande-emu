46c57de6ab71fee1daf2c8ebc2c0b4fd8420effb
Tim Yoong <timmoyoong678@protonmail.com>
2022-09-15 21:44:06 +0800

    An app that handles temperature unit conversions.

--- TempConvert/ContentView.swift
+++ TempConvert/ContentView.swift
@@ -2,20 +2,28 @@
 //  ContentView.swift
 //  TempConvert
 //
-//  Created by Timothy on 15/09/2022.
+//  Created by Timothy on 08/09/2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputFigure = 0.0
+    @State private var inputUnit = ["°C", "°F", "K", "°R"]
+    @State private var outputUnit = ["°C", "°F", "K", "°R"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Temperature unit to convert from", selection: $inputUnit) {
+                        ForEach(inputUnit, id: \.self){
+                            Text($0, format: .number)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +32,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
