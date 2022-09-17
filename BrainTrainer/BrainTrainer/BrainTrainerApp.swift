23d528d08f050708b3d6552999e89a6190168697
Istionia <79699038+Istionia@users.noreply.github.com>
2022-09-17 21:41:42 +0800

    Brain Trainer
    A brain training game that challenges players to win or lose at rock, paper, scissors.
    
    So, very roughly:
    
        - Each turn of the game the app will randomly pick either rock, paper, or scissors.
        - Each turn the app will alternate between prompting the player to win or lose.
        - The player must then tap the correct move to win or lose the game.
        - If they are correct they score a point; otherwise they lose a point.
        - The game ends after 10 questions, at which point their score is shown.

---
+++ BrainTrainer/BrainTrainer/BrainTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainerApp.swift
+//  BrainTrainer
+//
+//  Created by Timothy on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainTrainer/BrainTrainer/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  BrainTrainer
+//
+//  Created by Timothy on 17/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let moves = ["Rock", "Paper", "Scissors"]
+    @State private var currentMove = Int.random(in: 0...2)
+    
+    let outcomes = ["Win", "Lose"]
+    @State private var desiredOutcome = Bool.random()
+    
+    @State private var currentChoice = "Rock"
+    
+    var body: some View {
+        VStack {
+            Text("Rock, Paper, Scissors - or is it?")
+                .font(.largeTitle.bold())
+                .foregroundColor(.indigo)
+            
+            Spacer()
+            
+            VStack {
+                Text("Gamemaster's move is")
+                    .font(.subheadline.weight(.heavy))
+                    .foregroundStyle(.secondary)
+                
+                Text(moves[currentMove])
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.black)
+                
+                Spacer()
+                
+                Text("Gamemaster wants you to")
+                    .font(.subheadline.weight(.heavy))
+                    .foregroundStyle(.secondary)
+                
+                Text(outcomes[_desiredOutcome])
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.black)
+                
+                Spacer()
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
